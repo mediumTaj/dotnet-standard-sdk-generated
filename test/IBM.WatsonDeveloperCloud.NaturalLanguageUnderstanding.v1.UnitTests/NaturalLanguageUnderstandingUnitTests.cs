7d4a98307594ee752c35d93aff8c01f032f0ab75
Ajiemar Santiago <ajiemar@gmail.com>
2017-05-24 13:32:41 -0500

    starting nlu integration and unit tests

---
+++ test/IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.UnitTests/NaturalLanguageUnderstandingUnitTests.cs
@@ -0,0 +1,97 @@
+﻿/**
+* Copyright 2017 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+using System;
+using IBM.WatsonDeveloperCloud.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.UnitTests
+{
+    [TestClass]
+    public class NaturalLanguageUnderstandingUnitTests
+    {
+        #region Constructor
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_HttpClient_Null()
+        {
+            NaturalLanguageUnderstandingService service =
+                new NaturalLanguageUnderstandingService(null);
+        }
+
+        [TestMethod]
+        public void Constructor_HttpClient()
+        {
+            NaturalLanguageUnderstandingService service =
+                new NaturalLanguageUnderstandingService(CreateClient());
+
+            Assert.IsNotNull(service);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_UserName_Null()
+        {
+            NaturalLanguageUnderstandingService service =
+                new NaturalLanguageUnderstandingService(null, "password", NaturalLanguageUnderstandingService.NATURAL_LANGUAGE_UNDERSTANDING_VERSION_DATE_2017_02_27);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_Password_Null()
+        {
+            NaturalLanguageUnderstandingService service =
+                new NaturalLanguageUnderstandingService("username", null, NaturalLanguageUnderstandingService.NATURAL_LANGUAGE_UNDERSTANDING_VERSION_DATE_2017_02_27);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_Version_Null()
+        {
+            NaturalLanguageUnderstandingService service =
+                new NaturalLanguageUnderstandingService("username", "password", null);
+        }
+
+        [TestMethod]
+        public void Constructor_With_UserName_Password()
+        {
+            NaturalLanguageUnderstandingService service =
+                new NaturalLanguageUnderstandingService("username", "password", NaturalLanguageUnderstandingService.NATURAL_LANGUAGE_UNDERSTANDING_VERSION_DATE_2017_02_27);
+
+            Assert.IsNotNull(service);
+        }
+
+        [TestMethod]
+        public void Constructor()
+        {
+            NaturalLanguageUnderstandingService service =
+                new NaturalLanguageUnderstandingService();
+
+            Assert.IsNotNull(service);
+        }
+        #endregion
+
+        #region Create Client
+        private IClient CreateClient()
+        {
+            IClient client = Substitute.For<IClient>();
+
+            client.WithAuthentication(Arg.Any<string>(), Arg.Any<string>())
+                    .Returns(client);
+
+            return client;
+        }
+        #endregion
+    }
+}
