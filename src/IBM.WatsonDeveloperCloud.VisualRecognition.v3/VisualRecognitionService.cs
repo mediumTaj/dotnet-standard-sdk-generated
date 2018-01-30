d0667a57580cbb61cce1ed325d3a7d340c64e3ee
Ajiemar <ajiemar@gmail.com>
2018-01-30 13:44:42 -0600

    adding vr

---
+++ src/IBM.WatsonDeveloperCloud.VisualRecognition.v3/VisualRecognitionService.cs
@@ -0,0 +1,334 @@
+/**
+* Copyright 2018 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using IBM.WatsonDeveloperCloud.Http;
+using IBM.WatsonDeveloperCloud.Http.Extensions;
+using IBM.WatsonDeveloperCloud.Service;
+using IBM.WatsonDeveloperCloud.VisualRecognition.v3.Model;
+using System;
+
+namespace IBM.WatsonDeveloperCloud.VisualRecognition.v3
+{
+    public class VisualRecognitionService : WatsonService, IVisualRecognitionService
+    {
+        const string SERVICE_NAME = "visual_recognition";
+        const string URL = "https://gateway-a.watsonplatform.net/visual-recognition/api";
+        private string _versionDate;
+        public string VersionDate
+        {
+            get { return _versionDate; }
+            set { _versionDate = value; }
+        }
+
+        /** The Constant VISUAL_RECOGNITION_VERSION_DATE_2016_05_20. */
+        public static string VISUAL_RECOGNITION_VERSION_DATE_2016_05_20 = "2016-05-20";
+
+        public VisualRecognitionService() : base(SERVICE_NAME, URL)
+        {
+            if(!string.IsNullOrEmpty(this.Endpoint))
+                this.Endpoint = URL;
+        }
+
+        public VisualRecognitionService(string userName, string password, string versionDate) : this()
+        {
+            if (string.IsNullOrEmpty(userName))
+                throw new ArgumentNullException(nameof(userName));
+
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password));
+
+            this.SetCredential(userName, password);
+            if(string.IsNullOrEmpty(versionDate))
+                throw new ArgumentNullException("versionDate cannot be null. Use 'VISUAL_RECOGNITION_VERSION_DATE_2016_05_20'");
+
+            VersionDate = versionDate;
+        }
+
+        public VisualRecognitionService(IClient httpClient) : this()
+        {
+            if (httpClient == null)
+                throw new ArgumentNullException(nameof(httpClient));
+
+            this.Client = httpClient;
+        }
+
+        public ClassifiedImages Classify(System.IO.Stream imagesFile = null, string parameters = null, string acceptLanguage = null, string imagesFileContentType = null)
+        {
+
+            if(string.IsNullOrEmpty(VersionDate))
+                throw new ArgumentNullException("versionDate cannot be null. Use 'VISUAL_RECOGNITION_VERSION_DATE_2016_05_20'");
+
+            ClassifiedImages result = null;
+
+            try
+            {
+                var formData = new MultipartFormDataContent();
+
+                if (imagesFile != null)
+                {
+                    var imagesFileContent = new ByteArrayContent((imagesFile as Stream).ReadAllBytes());
+                    System.Net.Http.Headers.MediaTypeHeaderValue contentType;
+                    System.Net.Http.Headers.MediaTypeHeaderValue.TryParse(imagesFileContentType, out contentType);
+                    imagesFileContent.Headers.ContentType = contentType;
+                    formData.Add(imagesFileContent, "images_file", "filename");
+                }
+
+                if (parameters != null)
+                {
+                    var parametersContent = new StringContent(parameters, Encoding.UTF8, HttpMediaType.TEXT_PLAIN);
+                    formData.Add(parametersContent, "parameters");
+                }
+
+                result = this.Client.WithAuthentication(this.UserName, this.Password)
+                                .PostAsync($"{this.Endpoint}/v3/classify")
+                                .WithArgument("version", VersionDate)
+                                .WithHeader("Accept-Language", acceptLanguage)
+                                .WithBodyContent(formData)
+                                .As<ClassifiedImages>()
+                                .Result;
+            }
+            catch(AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+
+            return result;
+        }
+        public DetectedFaces DetectFaces(System.IO.Stream imagesFile = null, string parameters = null, string imagesFileContentType = null)
+        {
+
+            if(string.IsNullOrEmpty(VersionDate))
+                throw new ArgumentNullException("versionDate cannot be null. Use 'VISUAL_RECOGNITION_VERSION_DATE_2016_05_20'");
+
+            DetectedFaces result = null;
+
+            try
+            {
+                var formData = new MultipartFormDataContent();
+
+                if (imagesFile != null)
+                {
+                    var imagesFileContent = new ByteArrayContent((imagesFile as Stream).ReadAllBytes());
+                    System.Net.Http.Headers.MediaTypeHeaderValue contentType;
+                    System.Net.Http.Headers.MediaTypeHeaderValue.TryParse(imagesFileContentType, out contentType);
+                    imagesFileContent.Headers.ContentType = contentType;
+                    formData.Add(imagesFileContent, "images_file", "filename");
+                }
+
+                if (parameters != null)
+                {
+                    var parametersContent = new StringContent(parameters, Encoding.UTF8, HttpMediaType.TEXT_PLAIN);
+                    formData.Add(parametersContent, "parameters");
+                }
+
+                result = this.Client.WithAuthentication(this.UserName, this.Password)
+                                .PostAsync($"{this.Endpoint}/v3/detect_faces")
+                                .WithArgument("version", VersionDate)
+                                .WithBodyContent(formData)
+                                .As<DetectedFaces>()
+                                .Result;
+            }
+            catch(AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+
+            return result;
+        }
+        public Classifier CreateClassifier(string name, System.IO.Stream classnamePositiveExamples, System.IO.Stream negativeExamples = null)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+            if (classnamePositiveExamples == null)
+                throw new ArgumentNullException(nameof(classnamePositiveExamples));
+
+            if(string.IsNullOrEmpty(VersionDate))
+                throw new ArgumentNullException("versionDate cannot be null. Use 'VISUAL_RECOGNITION_VERSION_DATE_2016_05_20'");
+
+            Classifier result = null;
+
+            try
+            {
+                var formData = new MultipartFormDataContent();
+
+                if (name != null)
+                {
+                    var nameContent = new StringContent(name, Encoding.UTF8, HttpMediaType.TEXT_PLAIN);
+                    formData.Add(nameContent, "name");
+                }
+
+                if (classnamePositiveExamples != null)
+                {
+                    var classnamePositiveExamplesContent = new ByteArrayContent((classnamePositiveExamples as Stream).ReadAllBytes());
+                    System.Net.Http.Headers.MediaTypeHeaderValue contentType;
+                    System.Net.Http.Headers.MediaTypeHeaderValue.TryParse("application/octet-stream", out contentType);
+                    classnamePositiveExamplesContent.Headers.ContentType = contentType;
+                    formData.Add(classnamePositiveExamplesContent, "classname_positive_examples", "filename");
+                }
+
+                if (negativeExamples != null)
+                {
+                    var negativeExamplesContent = new ByteArrayContent((negativeExamples as Stream).ReadAllBytes());
+                    System.Net.Http.Headers.MediaTypeHeaderValue contentType;
+                    System.Net.Http.Headers.MediaTypeHeaderValue.TryParse("application/octet-stream", out contentType);
+                    negativeExamplesContent.Headers.ContentType = contentType;
+                    formData.Add(negativeExamplesContent, "negative_examples", "filename");
+                }
+
+                result = this.Client.WithAuthentication(this.UserName, this.Password)
+                                .PostAsync($"{this.Endpoint}/v3/classifiers")
+                                .WithArgument("version", VersionDate)
+                                .WithBodyContent(formData)
+                                .As<Classifier>()
+                                .Result;
+            }
+            catch(AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+
+            return result;
+        }
+
+        public object DeleteClassifier(string classifierId)
+        {
+            if (string.IsNullOrEmpty(classifierId))
+                throw new ArgumentNullException(nameof(classifierId));
+
+            if(string.IsNullOrEmpty(VersionDate))
+                throw new ArgumentNullException("versionDate cannot be null. Use 'VISUAL_RECOGNITION_VERSION_DATE_2016_05_20'");
+
+            object result = null;
+
+            try
+            {
+                result = this.Client.WithAuthentication(this.UserName, this.Password)
+                                .DeleteAsync($"{this.Endpoint}/v3/classifiers/{classifierId}")
+                                .WithArgument("version", VersionDate)
+                                .As<object>()
+                                .Result;
+            }
+            catch(AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+
+            return result;
+        }
+
+        public Classifier GetClassifier(string classifierId)
+        {
+            if (string.IsNullOrEmpty(classifierId))
+                throw new ArgumentNullException(nameof(classifierId));
+
+            if(string.IsNullOrEmpty(VersionDate))
+                throw new ArgumentNullException("versionDate cannot be null. Use 'VISUAL_RECOGNITION_VERSION_DATE_2016_05_20'");
+
+            Classifier result = null;
+
+            try
+            {
+                result = this.Client.WithAuthentication(this.UserName, this.Password)
+                                .GetAsync($"{this.Endpoint}/v3/classifiers/{classifierId}")
+                                .WithArgument("version", VersionDate)
+                                .As<Classifier>()
+                                .Result;
+            }
+            catch(AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+
+            return result;
+        }
+
+        public Classifiers ListClassifiers(bool? verbose = null)
+        {
+
+            if(string.IsNullOrEmpty(VersionDate))
+                throw new ArgumentNullException("versionDate cannot be null. Use 'VISUAL_RECOGNITION_VERSION_DATE_2016_05_20'");
+
+            Classifiers result = null;
+
+            try
+            {
+                result = this.Client.WithAuthentication(this.UserName, this.Password)
+                                .GetAsync($"{this.Endpoint}/v3/classifiers")
+                                .WithArgument("version", VersionDate)
+                                .WithArgument("verbose", verbose)
+                                .As<Classifiers>()
+                                .Result;
+            }
+            catch(AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+
+            return result;
+        }
+
+        public Classifier UpdateClassifier(string classifierId, System.IO.Stream classnamePositiveExamples = null, System.IO.Stream negativeExamples = null)
+        {
+            if (string.IsNullOrEmpty(classifierId))
+                throw new ArgumentNullException(nameof(classifierId));
+
+            if(string.IsNullOrEmpty(VersionDate))
+                throw new ArgumentNullException("versionDate cannot be null. Use 'VISUAL_RECOGNITION_VERSION_DATE_2016_05_20'");
+
+            Classifier result = null;
+
+            try
+            {
+                var formData = new MultipartFormDataContent();
+
+                if (classnamePositiveExamples != null)
+                {
+                    var classnamePositiveExamplesContent = new ByteArrayContent((classnamePositiveExamples as Stream).ReadAllBytes());
+                    System.Net.Http.Headers.MediaTypeHeaderValue contentType;
+                    System.Net.Http.Headers.MediaTypeHeaderValue.TryParse("application/octet-stream", out contentType);
+                    classnamePositiveExamplesContent.Headers.ContentType = contentType;
+                    formData.Add(classnamePositiveExamplesContent, "classname_positive_examples", "filename");
+                }
+
+                if (negativeExamples != null)
+                {
+                    var negativeExamplesContent = new ByteArrayContent((negativeExamples as Stream).ReadAllBytes());
+                    System.Net.Http.Headers.MediaTypeHeaderValue contentType;
+                    System.Net.Http.Headers.MediaTypeHeaderValue.TryParse("application/octet-stream", out contentType);
+                    negativeExamplesContent.Headers.ContentType = contentType;
+                    formData.Add(negativeExamplesContent, "negative_examples", "filename");
+                }
+
+                result = this.Client.WithAuthentication(this.UserName, this.Password)
+                                .PostAsync($"{this.Endpoint}/v3/classifiers/{classifierId}")
+                                .WithArgument("version", VersionDate)
+                                .WithBodyContent(formData)
+                                .As<Classifier>()
+                                .Result;
+            }
+            catch(AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/IBM.WatsonDeveloperCloud.VisualRecognition.v3.IntegrationTests/VisualRecognitionServiceIntegrationTests.cs
@@ -0,0 +1,323 @@
+﻿/**
+* Copyright 2017 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+
+//  Uncomment to test without creating a new classifier each test. VisualStudio on Mac has a hard-coded 60 second
+//  timeout on tests so creating a classifier and waiting for training to update a classifier fails unless this
+//  is uncommented.
+//#define PERSISTENT_CLASSIFIER
+
+using IBM.WatsonDeveloperCloud.VisualRecognition.v3.Model;
+using IBM.WatsonDeveloperCloud.Http.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using IBM.WatsonDeveloperCloud.Util;
+
+namespace IBM.WatsonDeveloperCloud.VisualRecognition.v3.IntegrationTests
+{
+    [TestClass]
+    public class VisualRecognitionServiceIntegrationTests
+    {
+        VisualRecognitionService _visualRecognition;
+        private static string credentials = string.Empty;
+        private static string _apikey;
+        private static string _endpoint;
+        private string _imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Kittyply_edit1.jpg/1200px-Kittyply_edit1.jpg";
+        private string _faceUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/220px-President_Barack_Obama.jpg";
+        private string _localGiraffeFilePath = @"VisualRecognitionTestData/giraffe_to_classify.jpg";
+        private string _localFaceFilePath = @"VisualRecognitionTestData/obama.jpg";
+        private string _localGiraffePositiveExamplesFilePath = @"VisualRecognitionTestData/giraffe_positive_examples.zip";
+        private string _giraffeClassname = "giraffe";
+        private string _localTurtlePositiveExamplesFilePath = @"VisualRecognitionTestData/turtle_positive_examples.zip";
+        private string _turtleClassname = "turtle";
+        private string _localNegativeExamplesFilePath = @"VisualRecognitionTestData/negative_examples.zip";
+        private string _createdClassifierName = "dotnet-standard-test-integration-classifier";
+#if PERSISTENT_CLASSIFIER
+        private static string _createdClassifierId = "";
+#else
+        private string _createdClassifierId = "";
+#endif
+        AutoResetEvent autoEvent = new AutoResetEvent(false);
+
+#region Setup
+        [TestInitialize]
+        public void Setup()
+        {
+            Console.WriteLine(string.Format("\nSetting up test"));
+
+            if (string.IsNullOrEmpty(credentials))
+            {
+                try
+                {
+                    credentials = Utility.SimpleGet(
+                        Environment.GetEnvironmentVariable("VCAP_URL"),
+                        Environment.GetEnvironmentVariable("VCAP_USERNAME"),
+                        Environment.GetEnvironmentVariable("VCAP_PASSWORD")).Result;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(string.Format("Failed to get credentials: {0}", e.Message));
+                }
+
+                Task.WaitAll();
+
+                var vcapServices = JObject.Parse(credentials);
+                _endpoint = vcapServices["visual_recognition"]["url"].Value<string>();
+                _apikey = vcapServices["visual_recognition"]["api_key"].Value<string>();
+            }
+
+            _visualRecognition = new VisualRecognitionService(_apikey, _endpoint);
+            _visualRecognition.Client.BaseClient.Timeout = TimeSpan.FromMinutes(120);
+        }
+#endregion
+
+#region Tests
+        [TestMethod]
+        public void ClassifyGetSuccess()
+        {
+            Console.WriteLine(string.Format("Running test Classify GET"));
+
+            List<string> classifiers = new List<string>()
+            {
+                "default"
+            };
+
+            List<string> owners = new List<string>()
+            {
+                "IBM",
+                "me"
+            };
+
+            var result = _visualRecognition.Classify(_imageUrl, classifiers.ToArray(), owners.ToArray());
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Images);
+            Assert.IsTrue(result.Images.Count > 0);
+        }
+
+        [TestMethod]
+        public void ClassifyPostSuccess()
+        {
+            Console.WriteLine(string.Format("Running Test Classify POST"));
+            using (FileStream fs = File.OpenRead(_localGiraffeFilePath))
+            {
+                var result = _visualRecognition.Classify((fs as Stream).ReadAllBytes(), Path.GetFileName(_localGiraffeFilePath), "image/jpeg");
+
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(result.Images);
+                Assert.IsTrue(result.Images.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void DetectFacesGetSuccess()
+        {
+            Console.WriteLine(string.Format("Running Test DetectFaces GET"));
+            var result = _visualRecognition.DetectFaces(_faceUrl);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Images);
+            Assert.IsTrue(result.Images.Count > 0);
+        }
+
+        [TestMethod]
+        public void DetectFacesPostSuccess()
+        {
+            Console.WriteLine(string.Format("Running Test DetectFaces POST"));
+            using (FileStream fs = File.OpenRead(_localFaceFilePath))
+            {
+                var result = _visualRecognition.DetectFaces((fs as Stream).ReadAllBytes(), Path.GetFileName(_localFaceFilePath), "image/jpeg");
+
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(result.Images);
+                Assert.IsTrue(result.Images.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void GetClassifiersBriefSuccess()
+        {
+            Console.WriteLine(string.Format("Running Test GetClassifiersBrief"));
+            var results = _visualRecognition.GetClassifiersBrief();
+
+            Assert.IsNotNull(results);
+        }
+
+        [TestMethod]
+        public void GetClassifiersVerboseSuccess()
+        {
+            Console.WriteLine(string.Format("Running Test GetClassifiersVerbose"));
+            _visualRecognition = new VisualRecognitionService(_apikey, _endpoint);
+            var results = _visualRecognition.GetClassifiersVerbose();
+
+            Assert.IsNotNull(results);
+        }
+
+        [TestMethod]
+        public void CreateClassifierSuccess()
+        {
+            Console.WriteLine(string.Format("Running Test CreateClassifier"));
+            var createClassifierResponse = CreateClassifier();
+
+            Assert.IsNotNull(createClassifierResponse);
+
+#if PERSISTENT_CLASSIFIER
+            //  Wait for classifier to finish training
+            var isClassifierReady = IsClassifierReady(_createdClassifierId);
+            autoEvent.WaitOne();
+#endif
+        }
+
+        [TestMethod]
+        public void UpdateClassifierSuccess()
+        {
+            Console.WriteLine(string.Format("Running Test UpdateClassifier"));
+#if !PERSISTENT_CLASSIFIER
+            var createClassifierResponse = CreateClassifier();
+
+            //  Wait for classifier to finish training
+            IsClassifierReady(_createdClassifierId);
+            autoEvent.WaitOne();
+#endif
+
+            var updateClassiferResponse = UpdateClassifier(_createdClassifierId);
+
+            Assert.IsNotNull(updateClassiferResponse);
+
+
+        }
+
+        [TestMethod]
+        public void DeleteClassifierSuccess()
+        {
+            Console.WriteLine(string.Format("Running Test DeleteClassifier"));
+#if !PERSISTENT_CLASSIFIER
+            var createClassifierResponse = CreateClassifier();
+
+            //  Wait for classifier to finish training
+            IsClassifierReady(_createdClassifierId);
+            autoEvent.WaitOne();
+#endif
+
+            var deleteClassifierResponse = DeleteClassifier(_createdClassifierId);
+
+            Assert.IsNotNull(deleteClassifierResponse);
+        }
+#endregion
+
+#region Teardown
+        [TestCleanup]
+        public void Teardown()
+        {
+            Console.WriteLine(string.Format("Tearing down..."));
+#if !PERSISTENT_CLASSIFIER
+            if (!string.IsNullOrEmpty(_createdClassifierId))
+            {
+                try
+                {
+                    var getClassifierResult = _visualRecognition.GetClassifier(_createdClassifierId);
+                    if(getClassifierResult != null)
+                        DeleteClassifier(_createdClassifierId);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Classifier {0} not found: {1}", _createdClassifierId, e.Message);
+                    _createdClassifierId = null;
+                }
+
+            }
+#endif
+        }
+#endregion
+
+#region CreateClassifier
+        private GetClassifiersPerClassifierVerbose CreateClassifier()
+        {
+            using (FileStream positiveExamplesStream = File.OpenRead(_localGiraffePositiveExamplesFilePath), negativeExamplesStream = File.OpenRead(_localNegativeExamplesFilePath))
+            {
+                Console.WriteLine(string.Format("Attempting to create classifier {0}", _createdClassifierName));
+
+                Dictionary<string, byte[]> positiveExamples = new Dictionary<string, byte[]>();
+                positiveExamples.Add(_giraffeClassname, positiveExamplesStream.ReadAllBytes());
+
+                var result = _visualRecognition.CreateClassifier(_createdClassifierName, positiveExamples, negativeExamplesStream.ReadAllBytes());
+
+                _createdClassifierId = result.ClassifierId;
+                Console.WriteLine(string.Format("Created classifier {0}", _createdClassifierId));
+
+                return result;
+            }
+        }
+#endregion
+
+#region UpdateClassifier
+        private GetClassifiersPerClassifierVerbose UpdateClassifier(string classifierId)
+        {
+            using (FileStream positiveExamplesStream = File.OpenRead(_localTurtlePositiveExamplesFilePath))
+            {
+                Console.WriteLine(string.Format("Attempting to update classifier {0}", classifierId));
+
+                Dictionary<string, byte[]> positiveExamples = new Dictionary<string, byte[]>();
+                positiveExamples.Add(_turtleClassname, positiveExamplesStream.ReadAllBytes());
+
+                var result = _visualRecognition.UpdateClassifier(classifierId, positiveExamples);
+                Console.WriteLine(string.Format("Updated classifier {0}", classifierId));
+
+                return result;
+            }
+        }
+#endregion
+
+#region DeleteClassifier
+        private object DeleteClassifier(string classifierId)
+        {
+            Console.WriteLine(string.Format("Attempting to delete classifier {0}", classifierId));
+            //Delay(_delayTime);
+            var result = _visualRecognition.DeleteClassifier(classifierId);
+            _createdClassifierId = null;
+            Console.WriteLine(string.Format("Deleted classifier {0}", classifierId));
+
+            return result;
+        }
+#endregion
+
+#region IsClassifierReady
+        private void IsClassifierReady(string classifierId)
+        {
+            var getClassifierResponse = _visualRecognition.GetClassifier(classifierId);
+
+            string status = getClassifierResponse.Status.ToLower();
+            Console.WriteLine(string.Format("Classifier status is {0}", status));
+
+            if (status == "ready")
+                autoEvent.Set();
+            else
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    IsClassifierReady(classifierId);
+                });
+            }
+        }
+#endregion
+    }
+}
